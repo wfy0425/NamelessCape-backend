234de6db8fb6c6630fe69e6f893727e5d5cec733
Fengyuan Wu <wfy0425@yeah.net>
2020-05-13 22:54:03 -0700

    Initial commit

---
+++ src/main/java/org/ucsdcssa/capes/CapesApplication.java
@@ -0,0 +1,13 @@
+package org.ucsdcssa.capes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ucsdcssa/capes/CapesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ucsdcssa.capes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
